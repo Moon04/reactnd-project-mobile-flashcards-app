e1fc0d15078f0dad4d636084f5e4da5f4b6ccbbe
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-23 18:40:18 +0200

    add changes for app.js

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
 
-export default function App() {
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import CustomStatusBar from "./components/CustomStatusBar";
+import AddDeck from "./components/AddDeck";
+import AddCard from "./components/AddCard";
+import Deck from "./components/Deck";
+import Decks from "./components/Decks";
+import Quiz from "./components/Quiz";
+
+import { setLocalNotification } from "./utils/helpers";
+import { purple } from "./utils/colors";
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Decks" component={Decks} />
+      <Tab.Screen name="Add Deck" component={AddDeck} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <CustomStatusBar backgroundColor={purple} barStyle="light-content" />
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="Deck" component={Deck} />
+              <Stack.Screen name="Add Card" component={AddCard} />
+              <Stack.Screen name="Quiz" component={Quiz} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
+  }
+}
